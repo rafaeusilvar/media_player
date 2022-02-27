56f46442a2f81dd33fd9ba705d242b74f13c5191
Rafael <rafaelsilvar74@gmail.com>
2022-02-26 22:19:16 -0300

    update config js

--- script.js
+++ script.js
@@ -10,24 +10,24 @@ function carregar(){
     msg.innerHTML = `Olá, bom-dia. Já conferiu a playlist do momento?`
     //BOM DIA!
   } else if (hora >= 12 && hora < 18){
-    msg.innerHTML = `Boa-tarde, Natasha Costa. Bem-vindo a Sun Music`
+    msg.innerHTML = `Boa-tarde, bem-vindo a Sun Music`
     //BOA TARDE!
   } else if (hora >=19){
-    msg.innerHTML = `Boa-noite, Natasha. Ouça a playlist do momento`
+    msg.innerHTML = `Boa-noite, ouça a playlist do momento`
     //BOA NOITE!
   } 
   }
-
-
-let musicas = [
-  {titulo:'SICKO MODE', artista:'Travis Scott', src:'musicas/Travis Scott - SICKO MODE (Audio).mp3', img:'https://www.vagalume.com.br/travis-scott/discografia/astroworld.jpg'},
+  
+  
+  let musicas = [
+  {titulo:'Passionfruit', artista:'Drake', src:'musicas/Drake - Passionfruit.mp3', img:'https://img.discogs.com/67AvKTmfkpr4K5UcJK87el7mQ_g=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-9999158-1538155037-8069.jpeg.jpg'},
   {titulo:'Company', artista:'Justin Bieber', src:'musicas/Company.mp3', img:'https://www.vagalume.com.br/justin-bieber/discografia/purpose.jpg'},
+  {titulo:'SICKO MODE', artista:'Travis Scott', src:'musicas/Travis Scott - SICKO MODE (Audio).mp3', img:'https://www.vagalume.com.br/travis-scott/discografia/astroworld.jpg'},
   {titulo:'Suit & Tie', artista:'Justin Timberlake', src:'musicas/Justin Timberlake - Suit & Tie (Official Video - Explicit) ft. Jay-Z.mp3', img:'http://3.bp.blogspot.com/-5iGUQ_LlMdY/UVF3LKRLXyI/AAAAAAAAU-g/nkmKt8cqkYA/s1600/The_20_20_Experience_(Deluxe_Version).jpg'},
   {titulo:'777-666', artista:'Matuê', src:'musicas/Matuê - 777-666.mp3', img:'https://www.vagalume.com.br/matue/discografia/maquina-do-tempo-11.jpg'},
   {titulo:'Wonderwall', artista:'Oasis', src:'musicas/Oasis-Wonderwall.mp3', img:'http://crownnote.com/sites/default/files/styles/album_artwork__300x300_/public/imgres_0.jpg?itok=Rm86sjlZ'},
   {titulo:'Smells Like Teen Spirit', artista:'Nirvana', src:'musicas/Nirvana - Smells Like Teen Spirit (Official Music Video).mp3', img:'https://regards.com.br/wp-content/uploads/2005/09/cd-nirvana-nevermind-nirvana-nevermind-00602527779089-2660252777908-300x300.jpg'},
   {titulo:'Uprising', artista:'Muse', src:'musicas/Muse - Uprising [Official Video].mp3', img:'https://i.pinimg.com/474x/f9/ce/09/f9ce09fcb1b8d6ce477582b81372eef9--best-album-covers-band-of.jpg'},
-  {titulo:'Passionfruit', artista:'Drake', src:'musicas/Drake - Passionfruit.mp3', img:'https://img.discogs.com/67AvKTmfkpr4K5UcJK87el7mQ_g=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-9999158-1538155037-8069.jpeg.jpg'},
   {titulo:'Its Time', artista:'Imagine Dragons', src:'musicas/Imagine Dragons - Its Time.mp3', img:'https://upload.wikimedia.org/wikipedia/pt/3/3f/Night_Visions_Album_Cover.jpeg'},
   {titulo:'New Light', artista:'John Mayer', src:'musicas/John Mayer - New Light (Official Audio).mp3', img:'https://t2.genius.com/unsafe/300x0/https%3A%2F%2Fimages.genius.com%2F840d3c8f34935e887790002123cefd0b.1000x1000x1.jpg'},
   // {titulo:'', artista:'', src:'', img:''},
@@ -55,6 +55,7 @@ renderizarMusica(indexMusica);
 //Eventos
 
 document.querySelector('.play-1').addEventListener('click', tocarMusica);
+document.querySelector('.proxima').addEventListener('click', tocarMusica);
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
